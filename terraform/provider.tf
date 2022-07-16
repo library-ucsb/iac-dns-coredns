ad37f02911a948cc29f28e205ded7f950749093a
Josh <josh@joshpreston.net>
2022-07-15 21:18:27 -0700

    added aws provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,21 @@
-
-
 terraform {
   required_providers {
     github = {
       source = "integrations/github"
       version = "4.26.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.22.0"
+    }
   }
 }
 
 provider "github" {
-  owner = var.github_owner
+  owner = var.repo.github_organization
+}
+
+provider "aws" {
+  region = var.aws_region
 }
 
